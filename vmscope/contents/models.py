d946bf6478db6560d3df3f64bd7cc9c747c1a2fa
Likit Preeyanon <likit.pre@mahidol.edu>
2018-08-07 17:32:21 +0700

    added ArticlePage model with some fields

--- vmscope/contents/models.py
+++ vmscope/contents/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.images.blocks import ImageChooserBlock
+
+class ArticlePage(Page):
+    author = models.CharField(max_length=255)
+    date = models.DateField("Created Date", auto_now_add=True)
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ])
+
+    content_panels = Page.content_panels + [
+        FieldPanel('author'),
+        FieldPanel('date'),
+        StreamFieldPanel('body'),
+    ]
